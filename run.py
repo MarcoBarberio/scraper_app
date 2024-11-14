838218cea0f4662ffb3cfb397263655d01675976
Marco <mbarberio23@gmail.com>
2024-11-14 16:11:34 +0100

    implementazione ai

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, jsonify, render_template
 from script.scraper.scraper import Scraper
 from script.text_generator.text_generation import Text_generator
+import google.generativeai as gemini
+import os
 app = Flask(__name__)
 
 @app.route('/')
@@ -8,7 +10,7 @@ def home():
     return render_template("index.html")
 
 
-@app.route('/search', methods=['POST', 'GET'])
+@app.route('/search', methods=['POST'])
 def search():
     data=request.get_json()
     url=data["url"]
@@ -17,6 +19,17 @@ def search():
     page_data=scraper.get_data(url)
     if page_data["response_code"]!=200:
         return jsonify({"result":"Error during the search execution"})
-    return jsonify({"result":page_data["plain_text"]})
+    text = page_data.get("plain_text", "")
+
+    if not text:
+        return jsonify({"result": "No text found on the page."})
+    generator=Text_generator()
+    result=generator.query("I will give you a text followed by a query.\n"+
+                           "you have to respond to the query using only informations that you can find on the text\n"+
+                           "the text begins and finishes with [TXT] placeholder\n"+
+                           "the query begins and finishes with [QUERY] placeholder\n"+
+                           "[TXT]"+text+"[TXT]\n"+
+                           "[QUERY]"+query+"[QUERY]")
+    return jsonify({"result":result.text})
 if __name__ == '__main__':
     app.run(debug=True)
--- script/text_generator/text_generation.py
+++ script/text_generator/text_generation.py
@@ -1,7 +1,7 @@
-from transformers import AutoModelForCausalLM, AutoTokenizer
 from .text_generation_interface import Text_generator_interface
+import google.generativeai as gemini
 import os
-import torch.cuda as cuda
+import re
 class Text_generator(Text_generator_interface):
      #il modello deve essere creato in singleton. 
     _instance = None 
@@ -14,18 +14,14 @@ class Text_generator(Text_generator_interface):
     def __init__(self):
         #nel caso sia la prima volta che viene chiamato il costruttore l'istanza non ha attributi
         if not hasattr(self, 'model'): 
-            self.model = AutoModelForCausalLM.from_pretrained("HuggingFaceTB/SmolLM2-1.7B-Instruct")
-            # con cuda si sposta il modello sulla gpu se abilitata
-            if os.getenv("USE_GPU"): 
-                if cuda.is_available():
-                    self.model.to("cuda")
-            self.tokenizer = AutoTokenizer.from_pretrained("HuggingFaceTB/SmolLM2-1.7B-Instruct")
-            self.tokenizer.pad_token_id=self.tokenizer.eos_token_id
+            gemini.configure(api_key=os.environ["GEMINI_API_KEY"])
+            self.model=gemini.GenerativeModel("gemini-1.5-flash")
     
     def query(self,prompt):
-        messages = [{"role": "user", "content": prompt}]
-        input_text=self.tokenizer.apply_chat_template(messages, tokenize=False)
-        inputs = self.tokenizer.encode(input_text, return_tensors="pt")
-        outputs = self.model.generate(inputs, max_new_tokens=50, temperature=0.2, top_p=0.9, do_sample=True)
-        # output è un insieme di token, quindi va decodificato
-        return self.tokenizer.decode(outputs[0], skip_special_tokens=True)
+        prompt=self._clean_text(prompt)
+        return self.model.generate_content(prompt)
+    
+    def _clean_text(self,text):
+        text = re.sub(r'\s+', ' ', text)
+        text = text.strip()
+        return text
